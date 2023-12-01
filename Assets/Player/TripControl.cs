3edd3f6b1451d33853214645db5d9389070f5f85
Michael Sibrava <48391846+psynut@users.noreply.github.com>
2023-12-01 10:54:04 -0800

    Switched TripControl's restart of PlayerMovement to Script

--- Assets/Player/TripControl.cs
+++ Assets/Player/TripControl.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class TripControl : MonoBehaviour
 {
+    [SerializeField]
+    private float movementPauseTime = 2.5f;
+
     private PlayerMovement playerMovement;
     private Animator animator;
 
     private void Awake() {
         playerMovement = GetComponent<PlayerMovement>();
         animator = GetComponent<Animator>();
-        animator.enabled = false;
     }
 
     private void Start() {
@@ -18,18 +20,20 @@ public class TripControl : MonoBehaviour
     }
 
     public void Trip() {
-        animator.enabled = true;
+        Debug.Log("Trip called");
         if(playerMovement != null) {            //Sometimes this is called when the End of Game process has already happened and destroys playerMovement.
             playerMovement.enabled = false;
         }
         animator.SetTrigger("Trip");
+        Invoke(nameof(ReenablePlayerMovement),movementPauseTime);
     }
+
+
 
     public void ReenablePlayerMovement() {
         if(playerMovement != null) {            //Sometimes this is called when the End of Game process has already happened and destroys playerMovement.
             playerMovement.enabled = true;
         }
-        animator.enabled = false;
     }
 
 }
